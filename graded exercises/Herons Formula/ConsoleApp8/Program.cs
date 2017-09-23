8623453244bf56fe025d08715f10efc579b32eaa
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-09-22 23:18:03 -0400

    Add files via upload

---
+++ graded exercises/Herons Formula/ConsoleApp8/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        string a;
+        double b, c, d;
+        int n, i;
+        public void read()
+
+        {
+            Console.WriteLine("\n Please enter a value for a or type quit");
+            a = (Console.ReadLine());
+            if (a != "quit")
+            {
+                b = double.Parse(a);
+                b += b;
+                c = b / n;
+                n = n++;
+                return;
+            }
+
+            Console.WriteLine("The average sum is" + "" + c);
+            Console.ReadLine();
+        
+     
+                
+
+               
+            }
+
+
+        
+
+    
+
+
+
+
+
+                
+
+            
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Program q = new Program();
+            q.read();
+        }
+    }
+}
