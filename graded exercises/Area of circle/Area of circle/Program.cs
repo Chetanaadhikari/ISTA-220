5306b936070fd337ae6b56c53dcc63678a8a5568
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-09-22 23:16:50 -0400

    Add files via upload

---
+++ graded exercises/Area of circle/Area of circle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{ 
+    class Programs
+    {
+        double r;
+        public void read()
+        { 
+            Console.WriteLine("\n Provide the value of radius r:");
+           r = double.Parse(Console.ReadLine());
+
+        }
+
+        public void compute()
+        {
+            double a, p;
+            a = (r * r) * Math.PI;
+            Console.WriteLine("\n The area of the circle is" + a);
+            Console.ReadLine();
+            p = 2 * r * Math.PI;
+            Console.WriteLine("\n The circumference of the circle is" + p);
+            Console.ReadLine();
+            }
+
+
+
+
+        
+    
+        static void Main(string[] args)
+        {
+        Programs a = new Programs();
+        a.read();
+        a.compute();
+        }
+    }
+
+}
