ab88bfab0b5f408b488a08d9a31d079db7090310
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-09-22 23:18:50 -0400

    Add files via upload

---
+++ graded exercises/AverageofNumbersOfScoreDeterminedByUser/ConsoleApp15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = 0;
+            Console.WriteLine("\n Write the number of scores you want to calculate the average for: ");
+            int n = int.Parse(Console.ReadLine());
+            int g = n -1;
+            Console.WriteLine("\n Enter the test score: ");
+            double x = double.Parse(Console.ReadLine());
+            y += x;
+
+            while (g > 0)
+            {
+                g--;
+                
+                Console.WriteLine("\n Enter the next test score: ");
+                x = double.Parse(Console.ReadLine());
+                y += x;
+            }
+
+            double z = y / n;
+            Console.WriteLine("\n The average of the" + "" + n + "" + "testscores you entered is" + " " + z + ".");
+            Console.ReadLine();
+        }
+
+    }
+}
