62d65e41a9df6e63ea85dcf7b1de03ea0cd04590
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-09-22 23:19:37 -0400

    Add files via upload

---
+++ graded exercises/AverageofNnumbers/ConsoleApp9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        string a;
+        double b, c, d;
+        int n;
+        public void crazy()
+        {
+
+            while (a != "done" )
+            {
+                Console.WriteLine("\n Please enter another number, and type done when you are done)");
+                a = Console.ReadLine();
+                b = Convert.ToDouble(a);
+                d = 0;
+                d += b;
+                n = 0;
+                n = n++;
+                c = d / n;
+            }
+
+            Console.WriteLine("The average is" + " " + c);
+            Console.ReadLine();
+        }
+
+       
+
+
+    
+        static void Main(string[] args)
+        {
+            Program c = new Program();
+            c.crazy();
+        }
+
+    }
+}
