9224a6a66d10e6cff598f8c4013a363ba4ac3cf0
Chetanaadhikari <31009266+Chetanaadhikari@users.noreply.github.com>
2017-10-01 16:11:37 -0400

    Add files via upload

---
+++ graded exercises/EncryptionPart3usingOOP/EncryptionPart3usingOOP/Userinputconverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionPart3usingOOP
+{
+    class Userinputconverter
+    {
+        public static string stringfilter(string input)
+        {
+            string encrypt = input.ToUpper();
+
+            string filtered = String.Join("", encrypt.Where(char.IsLetter));
+            return filtered;
+        }
+    }
+}
